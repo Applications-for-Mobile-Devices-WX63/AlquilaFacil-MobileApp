8c0dc8ef06129a4b1767b2a2188e46e6a8cd9553
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-09-14 11:26:58 -0500

    refactor(Bottom App Bar): replaced it as indepent widget for improve the code maintenance

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,24 +1,16 @@
 
 import 'package:alquilafacil/theme/main_theme.dart';
 import 'package:alquilafacil/widgets/navigation_button.dart';
+import 'package:alquilafacil/widgets/screen_bottom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget{
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return  const Scaffold(
       bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget> [
-             Expanded(child:   NavigationButton(buttonIcon: Icons.search_outlined, onNavigate: ()=>{}, iconColor: MainTheme.background, backgroundColor: Colors.orangeAccent),),
-            Expanded(child:   NavigationButton(buttonIcon: Icons.notifications, onNavigate: ()=>{},iconColor: Colors.orangeAccent, backgroundColor: MainTheme.background),),
-             Expanded(child:  NavigationButton(buttonIcon: Icons.add, onNavigate:()=>{},iconColor: Colors.orangeAccent, backgroundColor: MainTheme.background,)),
-             Expanded(child:  NavigationButton(buttonIcon: Icons.calendar_month_outlined, onNavigate: ()=>{}, iconColor: Colors.orangeAccent, backgroundColor: MainTheme.background)),
-              Expanded(child: NavigationButton(buttonIcon: Icons.person_outline, onNavigate: ()=>{},iconColor: Colors.orangeAccent, backgroundColor: MainTheme.background))
-            ],
-          ),
+          child: ScreenBottomAppBar()
       ),
     );
   }
---
+++ lib/widgets/screen_bottom_app_bar.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+import '../theme/main_theme.dart';
+import 'navigation_button.dart';
+
+class ScreenBottomAppBar extends StatelessWidget{
+  const ScreenBottomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget> [
+        Expanded(child:   NavigationButton(buttonIcon: Icons.search_outlined, onNavigate: ()=>{}, iconColor: MainTheme.background, backgroundColor: Colors.orangeAccent),),
+        Expanded(child:   NavigationButton(buttonIcon: Icons.notifications, onNavigate: ()=>{},iconColor: Colors.orangeAccent, backgroundColor: MainTheme.background),),
+        Expanded(child:  NavigationButton(buttonIcon: Icons.add, onNavigate:()=>{},iconColor: Colors.orangeAccent, backgroundColor: MainTheme.background,)),
+        Expanded(child:  NavigationButton(buttonIcon: Icons.calendar_month_outlined, onNavigate: ()=>{}, iconColor: Colors.orangeAccent, backgroundColor: MainTheme.background)),
+        Expanded(child: NavigationButton(buttonIcon: Icons.person_outline, onNavigate: ()=>{},iconColor: Colors.orangeAccent, backgroundColor: MainTheme.background))
+      ],
+    );
+  }
+
+}
