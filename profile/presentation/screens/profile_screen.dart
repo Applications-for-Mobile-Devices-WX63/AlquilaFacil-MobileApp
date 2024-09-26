85b8a7d622fdf2d252d8ab1d9c03924a6699f9c2
MarianoAmes <129983231+MarianoAmes@users.noreply.github.com>
2024-09-26 16:37:10 -0500

    feat(calendar): add profile screen

---
+++ profile/presentation/screens/profile_screen.dart
@@ -0,0 +1,49 @@
+import 'package:alquilafacil/profile/presentation/widgets/navigation_row.dart';
+import 'package:alquilafacil/public/presentation/widgets/screen_bottom_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Panel de control')),
+      body: const Center(
+          child: Card(
+            margin: EdgeInsets.all(16.0),
+            elevation: 10.0,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 32.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  NavigationRow(
+                    title: 'Modificar perfil',
+                    routeName: '/search',
+                  ),
+                  Divider(),
+                  NavigationRow(
+                    title: 'Destacar espacios',
+                    routeName: '/notifications',
+                  ),
+                  Divider(),
+                  NavigationRow(
+                    title: 'Gestionar espacios',
+                    routeName: '/calendar',
+                  ),
+                  Divider(),
+                  NavigationRow(
+                    title: 'Cerrar sesión',
+                    routeName: '/add',
+                  ),
+                ],
+              ),
+            ),
+          )
+      ),
+      bottomNavigationBar: const ScreenBottomAppBar(),
+    );
+  }
+}
