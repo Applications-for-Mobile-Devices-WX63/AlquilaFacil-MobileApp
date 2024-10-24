6c4ce0f51887a690758f408ea2c39069ea70486a
MarianoAmes <129983231+MarianoAmes@users.noreply.github.com>
2024-10-23 21:29:46 -0500

    feat(profile): update profile_provider

--- lib/profile/presentation/providers/pofile_provider.dart
+++ lib/profile/presentation/providers/pofile_provider.dart
@@ -4,11 +4,24 @@ import 'package:logger/logger.dart';
 
 class ProfileProvider extends ChangeNotifier{
   String usernameExpect = "";
+  List<dynamic> usernamesExpect = [];
   final UserServiceHelper userServiceHelper;
   ProfileProvider(this.userServiceHelper);
   Future<void> fetchUsernameExpect(int userId) async{
     try{
       usernameExpect = await userServiceHelper.getUsernameByUserId(userId);
+    } catch(_){
+      Logger().e("Error while trying to fetch username");
+    }
+    notifyListeners();
+  }
+
+  Future<void> fetchUsernamesExpect(List<dynamic> userIds) async{
+    try{
+      for (int i = 0; i < userIds.length; i++){
+        usernameExpect = await userServiceHelper.getUsernameByUserId(userIds[i]);
+        usernamesExpect.add(usernameExpect);
+      }
     } catch(_){
       Logger().e("Error while trying to fetch username");
     }
